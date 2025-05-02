f83c3dfe48a895a5b43481349dc14664132df8c7
AliLinaboui01 <alilinaboui@gmail.com>
2025-05-02 16:29:34 +0100

    correct array encoding tests

--- avro-serde/src/test/java/io/micronaut/avro/serde/AvroEncoderTest.java
+++ avro-serde/src/test/java/io/micronaut/avro/serde/AvroEncoderTest.java
@@ -10,21 +10,21 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AvroEncoderTest {
 
     @Test
     void testEncodeStringValidInput() throws IOException {
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        try(AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder)){
+
+        try ( AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder) ) {
             avroEncoder.encodeString("foo");
         }
-        encoder.flush();
+
         byte[] encodedBytes = outputStream.toByteArray();
         assertEquals("[6, 102, 111, 111]", Arrays.toString(encodedBytes));
         System.out.println(Arrays.toString(encodedBytes));
@@ -32,12 +32,14 @@ public class AvroEncoderTest {
 
     @Test
     void testEncodeStringEmptyString() throws IOException {
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        try(AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder)){
+
+        try ( AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder) ) {
             avroEncoder.encodeString("");
         }
-        encoder.flush();
+
         byte[] encodedBytes = outputStream.toByteArray();
         System.out.println(Arrays.toString(encodedBytes));
         assertEquals("[0]", Arrays.toString(encodedBytes));
@@ -45,22 +47,25 @@ public class AvroEncoderTest {
 
     @Test
     void testEncodeIntValidInput() throws IOException {
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        try(AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder)){
+
+        try ( AvroSerdeEncoder avroEncoder = new AvroSerdeEncoder(encoder) ){
             avroEncoder.encodeInt(10);
         }
-        encoder.flush();
+
         byte[] encodedBytes = outputStream.toByteArray();
         assertEquals("[20]", Arrays.toString(encodedBytes));
     }
 
     @Test
     void testSalamanderEncoder() throws IOException {
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        try(AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)){
 
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             encoder.encodeKey("name");
             encoder.encodeString("foo");
             encoder.encodeKey("age");
@@ -73,15 +78,15 @@ public class AvroEncoderTest {
             encoder.encodeBigDecimal(new BigDecimal("123.456"));
 
         }
-        binaryEncoder.flush();
+
         byte[] encodedBytes = outputStream.toByteArray();
         assertEquals("[24, 14, 49, 50, 51, 46, 52, 53, 54, 1, 0, 0, -72, 65, 6, 102, 111, 111]", Arrays.toString(encodedBytes));
 
     }
 
-
     @Test
     public void serializeArray() throws IOException {
+
           class Point {
             private final int x, y;
 
@@ -89,20 +94,13 @@ public class AvroEncoderTest {
                 this.x = x;
                 this.y = y;
             }
-
-            public int[] coords() {
-                return new int[] { x, y };
-            }
-
-            public Point valueOf(int x, int y) {
-                return new Point(x, y);
-            }
           }
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+
         // try-with-resource
-        try(AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)){
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             Argument<? extends Point> type = Argument.of(Point.class);
             Point value = new Point(3, 27);
             int[] coords = {2, 3, 5, 1, 5, 9};
@@ -115,26 +113,24 @@ public class AvroEncoderTest {
                 array.encodeInt(coords[5]);
 
             }
-            binaryEncoder.flush();
-            // Inspect raw bytes
-            byte[] actualResult = outputStream.toByteArray();
+        }
+        // Inspect raw bytes
+        byte[] actualResult = outputStream.toByteArray();
             Assertions.assertNotNull(actualResult);
             Assertions.assertTrue(actualResult.length > 0);
-            String expectedBytes = "[12, 4, 6, 10, 2, 10, 18, 0]";
+        String expectedBytes = "[12, 4, 6, 10, 2, 10, 18, 0]";
 
-            assertEquals(expectedBytes, Arrays.toString(actualResult));
-        }
+        assertEquals(expectedBytes, Arrays.toString(actualResult));
     }
 
     @Test
     public void serializeObject() throws IOException {
+
         class Male {
 
             private String name;
             private int lag;
 
-            public Male() {
-            }
             public Male(String name, int age) {
                 this.name = name;
                 this.lag = age;
@@ -150,8 +146,9 @@ public class AvroEncoderTest {
         }
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(out, null);
+
         // try-with-resource
-        try(AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)){
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             Argument<? extends Male> type = Argument.of(Male.class);
             Male male = new Male("foo", 23);
 
@@ -161,45 +158,45 @@ public class AvroEncoderTest {
                 array.encodeKey("age");
                 array.encodeInt(male.getAge());
             }
-            binaryEncoder.flush();
-            // Inspect raw bytes
-            byte[] avroData = out.toByteArray();
-            Assertions.assertNotNull(avroData);
-            Assertions.assertTrue(avroData.length > 0);
-            String actualResult = Arrays.toString(avroData);
-
-            assertEquals("[46, 6, 102, 111, 111]", actualResult);
-
         }
+
+        // Inspect raw bytes
+        byte[] avroData = out.toByteArray();
+        Assertions.assertNotNull(avroData);
+        Assertions.assertTrue(avroData.length > 0);
+        String actualResult = Arrays.toString(avroData);
+
+        assertEquals("[46, 6, 102, 111, 111]", actualResult);
     }
 
     @Test
     public void serializeSalamanderClass() throws IOException {
+
         // Define the Salamander class with Avro schema annotation
         @AvroSchemaSource("Salamander-schema.avsc")
         class Salamander {
             private String name;
             private List<List<String>> words;
             private int age;
-            private Set<String> stringSet;
+            private List<String> strings;
             private float salary;
 
-            public Salamander(String name, List<List<String>> words, int age, Set<String> stringSet, float salary) {
+            public Salamander(String name, List<List<String>> words, int age, List<String> stringSet, float salary) {
                 this.name = name;
                 this.words = words;
                 this.age = age;
-                this.stringSet = stringSet;
+                this.strings = stringSet;
                 this.salary = salary;
             }
         }
 
         // Initialize an instance of Salamander
-        Salamander salamanderInstance = new Salamander("ali", List.of(List.of("now", "how", "law")), 23, Set.of("bar", "foo"), 2100f);
+        Salamander salamanderInstance = new Salamander("ali", List.of(List.of("now", "how", "law")), 23, List.of("bar", "foo"), 2100f);
 
         // Output stream for the serialized data
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(out, null);
-        try (AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)) {
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             Argument<Salamander> type = Argument.of(Salamander.class);
             Encoder objectEncoder = encoder.encodeObject(type);
 
@@ -208,7 +205,7 @@ public class AvroEncoderTest {
             objectEncoder.encodeString(salamanderInstance.name);
 
             objectEncoder.encodeKey("words");
-            Encoder outerArrayEncoder = objectEncoder.encodeArray(Argument.of(String.class));
+            Encoder outerArrayEncoder = objectEncoder.encodeArray(Argument.of(List.class));
             for (List<String> innerList : salamanderInstance.words) {
 
                 Encoder innerArrayEncoder = outerArrayEncoder.encodeArray(Argument.of(String.class));
@@ -221,9 +218,9 @@ public class AvroEncoderTest {
             objectEncoder.encodeKey("age");
             objectEncoder.encodeInt(salamanderInstance.age);
 
-            objectEncoder.encodeKey("stringSet");
+            objectEncoder.encodeKey("strings");
             Encoder setEncoder = objectEncoder.encodeArray(Argument.of(String.class));
-            for (String item : salamanderInstance.stringSet) {
+            for (String item : salamanderInstance.strings) {
                 setEncoder.encodeString(item);
             }
 
@@ -236,7 +233,7 @@ public class AvroEncoderTest {
         // Validate the result
         byte[] encodedBytes = out.toByteArray();
         String actualResult = Arrays.toString(encodedBytes);
-        String expectedResult = "[6, 97, 108, 105, 2, 6, 6, 110, 111, 119, 6, 104, 111, 119, 6, 108, 97, 119, 0, 0, 46, 4, 6, 102, 111, 111, 6, 98, 97, 114, 0, 0, 64, 3, 69]";  // Replace with actual expected byte values
+        String expectedResult = "[6, 97, 108, 105, 2, 6, 6, 110, 111, 119, 6, 104, 111, 119, 6, 108, 97, 119, 0, 0, 46, 4, 6, 98, 97, 114, 6, 102, 111, 111, 0, 0, 64, 3, 69]";  // Replace with actual expected byte values
         assertEquals(expectedResult, actualResult);
     }
     @Test
@@ -257,24 +254,23 @@ public class AvroEncoderTest {
 
     @Test
     public void serializeArrays() throws IOException {
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-        try(AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)){
+
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             String[] strings = {"foo", "bar", "baz"};
             Argument<? extends String> argument = Argument.of(String.class);
             Encoder arrayEncoder = encoder.encodeArray(argument);
             arrayEncoder.encodeString(strings[0]);
             arrayEncoder.encodeString(strings[1]);
             arrayEncoder.encodeString(strings[2]);
-            arrayEncoder.finishStructure();
-
-            byte[] avroData = outputStream.toByteArray();
-            String actualResult = Arrays.toString(avroData);
-            String expectedResult = "[6, 6, 102, 111, 111, 6, 98, 97, 114, 6, 98, 97, 122, 0]";
-            System.out.println("Serialized data: " + Arrays.toString(avroData));
-            assertEquals(expectedResult, actualResult);
-
         }
+
+        byte[] avroData = outputStream.toByteArray();
+        String actualResult = Arrays.toString(avroData);
+        String expectedResult = "[6, 6, 102, 111, 111, 6, 98, 97, 114, 6, 98, 97, 122, 0]";
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
@@ -294,12 +290,12 @@ public class AvroEncoderTest {
         }
 
         // Initialize an instance of Salamander
-        Salamander salamanderInstance = new Salamander("ali", List.of(List.of("now", "how", "law")));
+        Salamander salamanderInstance = new Salamander("ali", List.of(List.of("now", "how", "law"), List.of("a", "b", "c")));
 
         // Output stream for the serialized data
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         org.apache.avro.io.Encoder binaryEncoder = EncoderFactory.get().binaryEncoder(out, null);
-        try (AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder)) {
+        try ( AvroSerdeEncoder encoder = new AvroSerdeEncoder(binaryEncoder) ) {
             Argument<Salamander> type = Argument.of(Salamander.class);
             Encoder objectEncoder = encoder.encodeObject(type);
 
@@ -315,16 +311,16 @@ public class AvroEncoderTest {
                 for (String word : innerList) {
                     innerArrayEncoder.encodeString(word);
                 }
+                innerArrayEncoder.finishStructure();
             }
+            outerArrayEncoder.finishStructure();
 
             objectEncoder.finishStructure();
         }
-
         // Validate the result
         byte[] encodedBytes = out.toByteArray();
         String actualResult = Arrays.toString(encodedBytes);
-        System.out.println("Serialized data: " + Arrays.toString(encodedBytes));
-        assertEquals("[6, 97, 108, 105, 2, 6, 6, 110, 111, 119, 6, 104, 111, 119, 6, 108, 97, 119, 0, 0]", actualResult);
+        assertEquals("[6, 97, 108, 105, 4, 6, 6, 110, 111, 119, 6, 104, 111, 119, 6, 108, 97, 119, 0, 6, 2, 97, 2, 98, 2, 99, 0, 0]", actualResult);
     }
 
 }
