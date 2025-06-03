0003165b088ffee570b781a2032273cd9596f30a
AliLinaboui01 <alilinaboui@gmail.com>
2025-06-03 12:09:28 +0100

    add test for mapper

--- avro-serde/src/test/java/io/micronaut/avro/serde/AvroObjectMapperTest.java
+++ avro-serde/src/test/java/io/micronaut/avro/serde/AvroObjectMapperTest.java
@@ -4,8 +4,8 @@ import io.micronaut.avro.Avro;
 import io.micronaut.avro.AvroSchemaSource;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.type.Argument;
+import io.micronaut.json.tree.JsonNode;
 import io.micronaut.serde.annotation.Serdeable;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -15,6 +15,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AvroObjectMapperTest {
     @Test
@@ -30,7 +32,7 @@ class AvroObjectMapperTest {
             mapper.writeValue(stream, Argument.of(MyBean.class), object);
             var deserialized = mapper.readValue(new ByteArrayInputStream(stream.toByteArray()), Argument.of(MyBean.class));
 
-            Assertions.assertEquals(object, deserialized);
+            assertEquals(object, deserialized);
         }
     }
 
@@ -68,7 +70,7 @@ class AvroObjectMapperTest {
             mapper.writeValue(stream, Argument.of(ComplexType.class), object);
             var deserialized = mapper.readValue(new ByteArrayInputStream(stream.toByteArray()), Argument.of(ComplexType.class));
 
-            Assertions.assertEquals(object, deserialized);
+            assertEquals(object, deserialized);
         }
     }
 
@@ -80,4 +82,110 @@ class AvroObjectMapperTest {
     ) {
 
     }
+    @Test
+    public void testNestedStructure() throws IOException {
+        NestedType object = new NestedType(List.of(List.of(List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz"))), MyBean.Color.GREEN);
+
+        try (ApplicationContext ctx = ApplicationContext.run()) {
+            AvroObjectMapper mapper = ctx.getBean(AvroObjectMapper.class);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            mapper.writeValue(stream, Argument.of(NestedType.class), object);
+            var deserialized = mapper.readValue(new ByteArrayInputStream(stream.toByteArray()), Argument.of(NestedType.class));
+
+            assertEquals(object, deserialized);
+        }
+    }
+
+    @Serdeable
+    @AvroSchemaSource("classpath:META-INF/avro-schemas/AvroObjectMapperTest/NestedType-schema.avsc")
+    @Avro
+    record NestedType(
+        List<List<List<String>>> nestedList,
+        MyBean.Color color
+    ) {
+
+    }
+
+    @Test
+    void testSimpleMapping() throws IOException {
+
+        Simple s = new Simple("test");
+        try (ApplicationContext ctx = ApplicationContext.run()) {
+            AvroObjectMapper mapper = ctx.getBean(AvroObjectMapper.class);
+            final byte[] result = mapper.writeValueAsBytes(s);
+            assertEquals("[8, 116, 101, 115, 116]", Arrays.toString(result));
+            final Simple simple = mapper.readValue(result, Argument.of(Simple.class));
+            assertNotNull(simple);
+            assertEquals(
+                "test",
+                simple.value()
+            );
+        }
+
+    }
+
+    @Serdeable
+    @AvroSchemaSource("classpath:META-INF/avro-schemas/AvroObjectMapperTest/Simple-schema.avsc")
+    @Avro
+    record Simple(String value) {}
+
+    @Test
+    void testWriteValueToTree() throws IOException {
+        try (ApplicationContext ctx = ApplicationContext.run()) {
+            AvroObjectMapper mapper = ctx.getBean(AvroObjectMapper.class);
+
+            Simple object = new Simple("test");
+
+            JsonNode jsonNode = mapper.writeValueToTree(object);
+            var deserializer = mapper.readValueFromTree(jsonNode, Simple.class);
+            assertNotNull(jsonNode);
+            assertTrue(jsonNode.isObject());
+            assertEquals("test", jsonNode.get("value").getStringValue());
+            assertEquals(object, deserializer);
+        }
+    }
+
+    @Test
+    void testWriteValueToTreeWithArgument() throws IOException {
+        try (ApplicationContext ctx = ApplicationContext.run()) {
+            AvroObjectMapper mapper = ctx.getBean(AvroObjectMapper.class);
+
+            Simple object = new Simple("test");
+
+            JsonNode jsonNode = mapper.writeValueToTree(Argument.of(Simple.class), object);
+            var deserialized = mapper.readValueFromTree(jsonNode, Argument.of(Simple.class));
+
+            assertNotNull(jsonNode);
+            assertTrue(jsonNode.isObject());
+            assertEquals("test", jsonNode.get("value").getStringValue());
+            assertEquals(object, deserialized);
+        }
+    }
+
+    @Test
+    public void testWriteValueWithOutputStreamAndObject() throws IOException {
+        try (ApplicationContext ctx = ApplicationContext.run()) {
+            AvroObjectMapper mapper = ctx.getBean(AvroObjectMapper.class);
+
+            Salamander salamander = new Salamander("salamander", List.of(List.of("foo", "bar"), List.of("baz")), 23, List.of("str1", "str2"), 2.1f);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            mapper.writeValue(stream, salamander);
+
+            var deserialized = mapper.readValue(new ByteArrayInputStream(stream.toByteArray()), Salamander.class);
+            assertEquals(salamander, deserialized);
+        }
+    }
+
+    @AvroSchemaSource("classpath:META-INF/avro-schemas/AvroObjectMapperTest/Salamander-schema.avsc")
+    @Avro
+    @Serdeable
+    record Salamander (
+        String name,
+        List<List<String>> words,
+        int age,
+        List<String> strings,
+        float salary
+    ){}
 }
